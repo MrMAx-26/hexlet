c73c0be13b967c8019fec6224f440b6c20d82cfa
Максим Шевцов <shevtsov_stv@mail.ru>
2025-01-23 13:05:08 +0300

    Add test_stack

---
+++ homework/hexlet_pytest/tests/test_stack.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+def test_stack():
+    stack = []
+    stack.append(1)
+    stack.append(2)
+    assert stack.pop() == 2
+    assert stack.pop() == 1
+
+
+def test_emptiness():
+    stack = []
+    assert not stack
+    stack.append(1)
+    assert bool(stack)
+    stack.pop()
+    assert not stack
+
+
+def test_pop_with_empy_stack():
+    stack = []
+    with pytest.raises(IndexError):
+        stack.pop()
